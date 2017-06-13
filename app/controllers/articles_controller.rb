a2193be073cac3c6b268016071a7f95f5c73cc0f
Sam <samantha.mar017@gmail.com>
2017-06-13 18:49:43 +0000

    Complete 5.11

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
     def new 
+        @article = Article.new
     end
     
     def index
@@ -13,9 +14,11 @@ class ArticlesController < ApplicationController
     def create
         #create a new article object with the information from the form
         @article = Article.new(article_params)
-        
-        @article.save 
-        redirect_to @article 
+        if @article.save 
+            redirect_to @article 
+        else
+            render 'new'
+        end
     end
     
     private 
